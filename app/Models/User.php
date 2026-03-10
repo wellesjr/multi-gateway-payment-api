cd75c78a11c0f061d62531954621cadd33004fbb
Welles Jr <welles-junior@hotmail.com>
2026-03-10 16:59:40 -0300

    fix: add comentarios e role

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
+
     /**
-     * The attributes that should be hidden for serialization.
+     * Os atributos que devem ser ocultados para serialização.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -34,9 +30,16 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Define as transformações de tipos de atributos do modelo.
      *
-     * @return array<string, string>
+     * Este método especifica como os atributos do modelo devem ser convertidos
+     * para seus tipos apropriados ao serem recuperados do banco de dados,
+     * e como devem ser armazenados.
+     *
+     * @return array<string, string> Um array associativo onde as chaves são nomes
+     *                               de atributos e os valores são os tipos de casting.
+     *                               - 'email_verified_at': converte para DateTime
+     *                               - 'password': aplica hash automáticamente
      */
     protected function casts(): array
     {
