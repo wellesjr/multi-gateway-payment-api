be247e09e8d2d7af30b40063c0192281f069b09c
Welles Jr <welles-junior@hotmail.com>
2026-03-10 16:35:26 -0300

    FIX: Adiciona tabelas de migração para gateways, clientes, produtos, transações e produtos de transação; adiciona coluna de função na tabela de usuários

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['ADMIN','MANAGER','FINANCE','USER'])->default('USER');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2026_03_10_192527_create_transactions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('gateway_id')->nullable()->constrained()->nullOnDelete();
+
+            $table->string('external_id')->nullable();
+
+            $table->enum('status', [
+                'pending',
+                'paid',
+                'failed',
+                'refunded',
+            ])->default('pending');
+
+            $table->integer('amount');
+
+            $table->string('card_last_numbers', 4)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
