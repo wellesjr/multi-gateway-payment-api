69134c7fa7760eb650336d58cdceda3aeb15060a
Welles Jr <welles-junior@hotmail.com>
2026-03-11 10:13:02 -0300

    FEAT: Adiciona arquivos de tradução para autenticação, paginação, redefinição de senha e validação

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Você redefiniu sua senha!',
+    'sent' => 'Enviamos um link para redefinir sua senha para o seu email.',
+    'throttled' => 'Por favor, espere antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com esse endereço de email.",
+
+];
