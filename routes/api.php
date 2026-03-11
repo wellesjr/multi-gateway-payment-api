e4bbcf371cee063cd1274f6e203b2e4096d1ad89
Welles Jr <welles-junior@hotmail.com>
2026-03-11 17:22:10 -0300

    feat: adicionar middleware de throttle na rota de login

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
 
 Route::post('/comprar', function () {
     return response()->json(['message' => 'Compra realizada com sucesso']);
